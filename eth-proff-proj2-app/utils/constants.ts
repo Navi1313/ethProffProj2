d96c72c3b02906915ff5f8a411dda5c17c97b42e
Navi <121182901+Navi1313@users.noreply.github.com>
2024-07-05 18:47:57 +0530

    Update constants.ts

--- eth-proff-proj2-app/utils/constants.ts
+++ eth-proff-proj2-app/utils/constants.ts
@@ -1,4 +1,4 @@
-import { createThirdwebClient, defineChain, getContract } from "thirdweb";
+import { createThirdwebClient, defineChain, getContract } from "thirdweb"
 
 const CLIENT_ID = process.env.NEXT_PUBLIC_CLIENT_ID
 
@@ -8,37 +8,115 @@ export const client  = createThirdwebClient({
 
 export const chain = defineChain(84532)
 
-export const contractAddress = "0x4e8fc04EBC76489730FBA00f2E01B653eA256073"
+export const contractAddress = "0x0A37C7Eb2dCa83C329311eA248f52abbb2C3719E"
 
 export const contractABI = [
-    {
-      "type": "function",
-      "name": "decCounter",
-      "inputs": [],
-      "outputs": [],
-      "stateMutability": "nonpayable"
-    },
-    {
-      "type": "function",
-      "name": "incCounter",
-      "inputs": [],
-      "outputs": [],
-      "stateMutability": "nonpayable"
-    },
-    {
-      "type": "function",
-      "name": "returnCount",
-      "inputs": [],
-      "outputs": [
-        {
-          "type": "uint256",
-          "name": "",
-          "internalType": "uint256"
-        }
-      ],
-      "stateMutability": "view"
-    }
-  ]  as const ;
+  {
+    "type": "function",
+    "name": "addQuantity",
+    "inputs": [
+      {
+        "type": "uint256",
+        "name": "_index",
+        "internalType": "uint256"
+      }
+    ],
+    "outputs": [],
+    "stateMutability": "nonpayable"
+  },
+  {
+    "type": "function",
+    "name": "addUniqueItems",
+    "inputs": [
+      {
+        "type": "string",
+        "name": "_name",
+        "internalType": "string"
+      },
+      {
+        "type": "uint256",
+        "name": "_price",
+        "internalType": "uint256"
+      },
+      {
+        "type": "uint256",
+        "name": "_quantity",
+        "internalType": "uint256"
+      }
+    ],
+    "outputs": [],
+    "stateMutability": "nonpayable"
+  },
+  {
+    "type": "function",
+    "name": "decreaseQuantity",
+    "inputs": [
+      {
+        "type": "uint256",
+        "name": "_index",
+        "internalType": "uint256"
+      }
+    ],
+    "outputs": [],
+    "stateMutability": "nonpayable"
+  },
+  {
+    "type": "function",
+    "name": "getTable",
+    "inputs": [],
+    "outputs": [
+      {
+        "type": "tuple[]",
+        "name": "",
+        "components": [
+          {
+            "type": "string",
+            "name": "name",
+            "internalType": "string"
+          },
+          {
+            "type": "uint256",
+            "name": "price",
+            "internalType": "uint256"
+          },
+          {
+            "type": "uint256",
+            "name": "quantity",
+            "internalType": "uint256"
+          }
+        ],
+        "internalType": "struct MySmallGarrrage.Items[]"
+      }
+    ],
+    "stateMutability": "view"
+  },
+  {
+    "type": "function",
+    "name": "removeItems",
+    "inputs": [
+      {
+        "type": "uint256",
+        "name": "_index",
+        "internalType": "uint256"
+      }
+    ],
+    "outputs": [],
+    "stateMutability": "nonpayable"
+  },
+  {
+    "type": "function",
+    "name": "totalItems",
+    "inputs": [],
+    "outputs": [
+      {
+        "type": "uint256",
+        "name": "",
+        "internalType": "uint256"
+      }
+    ],
+    "stateMutability": "view"
+  }
+] as const ;
 
   export const  CONTRACT  = getContract ({
     client : client, 
