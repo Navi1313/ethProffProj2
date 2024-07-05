51fea994ffbb5257a108e8cce516214f4e329f8e
Navi <121182901+Navi1313@users.noreply.github.com>
2024-07-05 18:52:46 +0530

    Update Contract.sol

--- eth-avax-proj2/contracts/Contract.sol
+++ eth-avax-proj2/contracts/Contract.sol
@@ -1,19 +1,70 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.9;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
-contract MyContract {
-    uint count ; 
+contract MySmallGarrrage {
+    
+    struct Items{
+        string name;
+        uint256 price;
+        uint256 quantity; 
+    }
+    struct ItemExists{
+        uint256 price;
+        bool exists;
+    }
+    // Mapping from ItemName -> ItemExists 
+    mapping(string => ItemExists) private ItemsMap ; 
 
-    function incCounter()  public {
-        count += 1 ;
+    // Array of structure type Containing Items 
+    Items [] private ItemsArray ;
+
+    // 1) Add Unique Items in The Garrage :->
+    function addUniqueItems(string memory _name , uint256 _price , uint256 _quantity) public{
+
+        require(!ItemsMap[_name].exists , "Item Already in Garage! you should add Items button below");
+        ItemsMap[_name] = ItemExists(_price  , true ) ;
+        ItemsArray.push(Items(_name ,_price , _quantity)) ;
+    } 
+
+    // 2) addQuantity of Items for particular Item :->
+    function addQuantity(uint256 _index) public {
+        string memory item_name = ItemsArray[_index].name ; 
+        require(ItemsMap[item_name].exists , "no such item exists"); 
+        ItemsArray[_index].quantity  += 1 ; 
     }
 
-    function decCounter() public {
-        require(count > 0 ,"counter can not be -ve ");
-        count -= 1 ;
+    // 3) Decrease Quantity :->
+    function decreaseQuantity(uint256 _index) public {
+        require(_index < ItemsArray.length, "Index out of bounds");
+        string memory item_name = ItemsArray[_index].name ;
+        require(ItemsMap[item_name].exists , "no such item exists");
+        ItemsArray[_index].quantity  -= 1 ;
     }
 
-    function getCount() public view returns(uint){
-        return count ; 
+    // 4) Remove Items :->
+    function removeItems(uint256 _index) public {
+        require(_index < ItemsArray.length, "Index out of bounds");
+        string memory item_name = ItemsArray[_index].name ;
+        require(ItemsMap[item_name].exists , "no such item exists");
+        delete ItemsMap[item_name] ;
+         // Swap and pop to remove element efficiently
+        ItemsArray[_index] = ItemsArray[ItemsArray.length - 1];
+        ItemsArray.pop();
+    }
+
+    // 5) Total Items in the Garage :->
+    function totalItems() public view returns(uint256){
+       uint256 count = 0 ; 
+        for(uint i = 0 ; i < ItemsArray.length ; i++){
+            if(ItemsArray[i].quantity > 0){
+                count += ItemsArray[i].quantity ;
+            } 
+        }
+        return count ;
+    }
+
+// 6) tABLE OF Total Items in the Garage :->
+    function getTable() public view returns (Items[] memory) {
+        return ItemsArray;
     }
 }
